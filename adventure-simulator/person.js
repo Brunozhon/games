ebc248e2331f740001c779c22eb15bb6a1451237
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2020-12-21 23:14:00 -0500

    Cool!

---
+++ adventure-simulator/person.js
@@ -0,0 +1,22 @@
+var person = {
+  calories: 300,
+  money: 100,
+  popularity: "Not known",
+  howpopular: 0,
+  moneypersec: 2
+};
+function checkPopular() {
+  if (person.howpopular == 0) {
+    person.popularity = "Not known";
+    person.moneypersec = 2;
+  } else if (person.howpopular <= 10) {
+    person.popularity = "Ony a few people know you";
+    person.moneypersec = 2 + person.howpopular;
+  } else if (person.howpopular <= 100) {
+    person.popularity = "Popular";
+    person.moneypersec = 10 + person.howpopular;
+  } else {
+    person.popularity = "The most popular person in the world";
+    person.moneypersec = 50 + person.howpopular;
+  }
+}
