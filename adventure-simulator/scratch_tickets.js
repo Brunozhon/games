67edfa15c9d7b623a7e18dd2a0a18d5347a43741
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2021-03-28 12:38:40 -0400

    DO THIS FOR NOW.

--- adventure-simulator/scratch_tickets.js
+++ adventure-simulator/scratch_tickets.js
@@ -1,7 +1,37 @@
 function rollTicket() {
   if (person.money <= 10) {
-    person.money = person.money - 10;
-    
+    person.money -= 10;
+    num = Math.random(); 
+    if (num > 9) {
+      output("You didn't earn anything. Better luck next time!")
+    } else if (num > 9 && num < 9.0001) {
+      output("You got a world map! Congratulations!")
+      person.foundWorldMap = true;
+    } else if (num > 9.0001 && num < 9.0002 ) {
+      output("You can roll another time for free!")
+      person.money += 10;
+      rollTicket();
+    } else if (num > 9.0002 && num < 9.0003) {
+      output("You just earned A LOT OF MONEY!")
+      person.money += 10000000000000000;
+    } else if (num > 9.0003 && num <  9.001) {
+      output("You lost 50 dollars. Better luck next time!")
+      person.money -= 50;
+    } else if (num > 9.001 && num < 9.0011) {
+      var f = Math.floor(Math.random() * 10) / 1;
+      var ft = Math.floor(Math.random() * 10) / 2;
+      var ftg = Math.floor(Math.random() * 10) / 5;
+      var v = f + ft + ftg;
+      var d = f + ftg / 2;
+      output("You got $" + v + " from your ticket minus $" + d + ". Congratulations!");
+      person.money += v - d;
+    } else if (num > 9.0011 && num < 9.0015) {
+      output('You earned $1. It might be sad, but as my mom says, "A little money is still money." She also says, "If you stash up money, you\'ll get more money."')
+      person.money += 1;  
+    } else if (num > 9.0015 && num < 9.0016) {
+      output("You got spiked armor. Congratulations! Now ypersomou can fight.");
+      person.inventory.push("spiked helmet", "spiked chestplate", "spiked pants", "spiked boots", "spiked sword", "spiked bow", "spiked arrow"); 
+    }
   } else {
     output("You don't have enough money to get a scratch ticket and use it.")
   }
