56fda7035cb0b2d556e79bb7e0a7feba3dbcb3b0
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2021-06-01 18:25:22 -0400

    Do this later

---
+++ real-life-simulator/jobs.js
@@ -0,0 +1,24 @@
+var jobs = [
+  {
+    name: "Pizza delivery",
+    moneywhenwork: 20,
+    description: "Pizza is yummy! Earn 20 dollars when you work to help yourself!",
+    requirements: ["none"]
+  },
+  {
+    name: "Scientist",
+    moneywhenwork: 500,
+    description: "Test expriments! Make things... by accident or on purporse! It's a fun job! The only disadvantage is that is requires college",
+    requirements: ["Master in science - College"]
+  },
+  {
+    name: "Teacher",
+    moneywhenwork: 250,
+    description: "Wow! You get to tell your students facts like 'Some elements are poisionus. If you get poisoned, you will suffer from...' Th only problems that you have are students blurting out 'How do you get poisoned?' and that you require college.",
+    requirements: ["Master in teaching - College"]
+  },
+  {
+    name: "President",
+    moneywhenwork: 1000
+  }
+]
